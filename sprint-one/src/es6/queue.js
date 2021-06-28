16ae6413ecf2d6a4c5bbafa6d01240001c74f0fc
Andre Jia <ajia.117@gmail.com>
2021-06-28 15:25:06 -0500

    implemented es6-syntax stack and queue

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -1,7 +1,23 @@
 class Queue {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.startIndex = 0;
+    this.endIndex = 0;
   }
 
+  enqueue(value) {
+    this.storage[this.endIndex] = value;
+    this.endIndex++;
+  }
+
+  dequeue() {
+    if(this.startIndex !== this.endIndex) {
+      this.startIndex++;
+    }
+    return this.storage[this.startIndex-1];
+  }
+
+  size() {
+    return this.endIndex - this.startIndex;
+  }
 }
