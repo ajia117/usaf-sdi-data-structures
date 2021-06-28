0472ce7e4a9975eeb79d75d29bdce4a819698fa9
Andre Jia <ajia.117@gmail.com>
2021-06-28 15:43:56 -0500

    implemented functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,27 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var newQueue = {
+    storage: {},
+    startIndex:0,
+    endIndex:0
+  }
+  Object.assign(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.endIndex] = value;
+  this.endIndex++;
+}
 
+queueMethods.dequeue = function() {
+  if(this.startIndex !== this.endIndex) {
+    this.startIndex++;
+  }
+  return this.storage[this.startIndex - 1];
+}
+
+queueMethods.size = function() {
+  return this.endIndex - this.startIndex;
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,35 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
-  const instance = {};
+  var instance = {};
   instance.storage = {};
-  instance.curentSize = 0;
+  instance.currentSize = 0;
   Object.assign(instance, stackMethods);
 
-  return instance
+  return instance;
 };
 
-var stackMethods = {
-
-  // Use an object with numeric keys to store values
+var stackMethods = {};
 
   // Implement the methods below
-  push: function(value) {
-    this.storage[this.currentSize] = value;
-    this.currentSize++;
-  },
-
-  pop: function() {
-    if(this.currentSize !== 0){
-      this.currentSize--
-    }
-    return this.storage[this.currentSize]
-  },
-
-  size: function() {
-    return this.currentSize
-  }
-
+stackMethods.push = function(value) {
+  this.storage[this.currentSize] = value;
+  this.currentSize++;
 };
 
+stackMethods.pop = function() {
+  if(this.currentSize !== 0){
+    this.currentSize--
+  }
+  return this.storage[this.currentSize]
+};
 
+stackMethods.size = function() {
+  return this.currentSize;
+};
